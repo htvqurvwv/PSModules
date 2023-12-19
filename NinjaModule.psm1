5f3005f79f26a35e103fa7f13dea26f2c8811f50
Alexi Peck <alexinicolaspeck@gmail.com>
2023-12-19 17:00:49 +1100

    Add function to get string from Ninja custom field, if error occurs or value doesn't exist, it will return $null

--- NinjaModule.psm1
+++ NinjaModule.psm1
@@ -12,6 +12,27 @@ function Get-NinjaPropertyBool {
     }
 }
 
+#returns string or $null
+#If Ninja-Property-Get is available but the field doesn't exist, it outputs "Unable to find the specified field." without a newline character,
+#there is currently nothing I can do to prevent this without affecting the output
+function Get-NinjaPropertyString {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$Field
+    )
+    try {
+        $value = Ninja-Property-Get $Field
+        if ($value -is [System.String] -and $value -ne "") {
+            return $value
+        } else {
+            return $null
+        }
+    } catch {
+        Write-Host "An error occurred function ""Get-NinjaPropertyString"", exiting with exception message ""$($_.Exception.Message)"""
+        exit 1
+    }
+}
+
 function DocumentServiceStates {
     param(
         [Parameter(Mandatory=$true)]
